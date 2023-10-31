c028a9825469c2b91182cb3f60040597347b704c
sakshi8009 <141905826+sakshi8009@users.noreply.github.com>
2023-10-31 11:47:03 +0530

    Add files via upload

---
+++ Serve_clie/server.js
@@ -0,0 +1,14 @@
+var net = require('net');
+var client = net.connect({port: 8080},function(){
+console.log("Connected to server");
+});
+client.on('data', function(data){
+console.log(data.toString());
+client.end();
+});
+client.on('end', function(){
+console.log("Disconnected");
+});
+client.on('error', function(err){
+console.log(err.toString());
+});
